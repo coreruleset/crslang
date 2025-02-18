f2f833edfbe294a4011d669544b554a97f7092b3
Agustindeleon <agustin.emanuel.de.leon@fing.edu.uy>
2025-02-17 23:48:38 -0300

    refactor: add config directives types

--- extended_seclang_parser_listener.go
+++ extended_seclang_parser_listener.go
@@ -89,7 +89,7 @@ func (l *ExtendedSeclangParserListener) EnterRule_script_directive(ctx *parsing.
 func (l *ExtendedSeclangParserListener) EnterEngine_config_directive_with_param(ctx *parsing.Engine_config_directive_with_paramContext) {
 	// fmt.Println("String engine config directive: ", ctx.GetText())
 	l.currentConfigurationDirective = types.NewConfigurationDirective()
-	l.currentConfigurationDirective.Name = ctx.GetText()
+	l.currentConfigurationDirective.SetName(ctx.GetText())
 	l.currentFunctionToAppendComment = l.currentConfigurationDirective.GetMetadata().SetComment
 	l.currentFunctionToAppendDirective = func() {
 		l.Configuration.Directives = append(l.Configuration.Directives, *l.currentConfigurationDirective)
@@ -107,7 +107,7 @@ func (l *ExtendedSeclangParserListener) EnterValues(ctx *parsing.ValuesContext) 
 
 func (l *ExtendedSeclangParserListener) EnterEngine_config_sec_cache_transformations(ctx *parsing.Engine_config_sec_cache_transformationsContext) {
 	l.currentConfigurationDirective = types.NewConfigurationDirective()
-	l.currentConfigurationDirective.Name = ctx.GetText()
+	l.currentConfigurationDirective.SetName(ctx.GetText())
 	l.currentFunctionToAppendComment = l.currentConfigurationDirective.GetMetadata().SetComment
 	l.currentFunctionToSetParam = func(value string) {
 		l.currentConfigurationDirective.Parameter = value
@@ -288,7 +288,7 @@ func (l *ExtendedSeclangParserListener) EnterOperator_value(ctx *parsing.Operato
 
 func (l *ExtendedSeclangParserListener) EnterString_engine_config_directive(ctx *parsing.String_engine_config_directiveContext) {
 	l.currentConfigurationDirective = types.NewConfigurationDirective()
-	l.currentConfigurationDirective.Name = ctx.GetText()
+	l.currentConfigurationDirective.SetName(ctx.GetText())
 	l.currentFunctionToAppendComment = l.currentConfigurationDirective.GetMetadata().SetComment
 	l.currentFunctionToAppendDirective = func() {
 		l.Configuration.Directives = append(l.Configuration.Directives, *l.currentConfigurationDirective)
@@ -304,7 +304,10 @@ func (l *ExtendedSeclangParserListener) EnterSec_marker_directive(ctx *parsing.S
 	// fmt.Println("Sec marker directive: ", ctx.GetText())
 	// l.currentParameter = ctx.GetText()
 	l.currentConfigurationDirective = types.NewConfigurationDirective()
-	l.currentConfigurationDirective.Name = ctx.GetText()
+	err := l.currentConfigurationDirective.SetName(ctx.GetText())
+	if err != nil {
+		panic(err)
+	}
 	l.currentFunctionToAppendComment = l.currentConfigurationDirective.GetMetadata().SetComment
 	l.currentFunctionToSetParam = func(value string) {
 		l.currentConfigurationDirective.Parameter = value
--- types/seclang_directives.go
+++ types/seclang_directives.go
@@ -1,5 +1,7 @@
 package types
 
+import "fmt"
+
 type SeclangDirective interface {
 	ToSeclang() string
 }
@@ -12,12 +14,167 @@ type ChainableDirective interface {
 	NonDisruptiveActionsCount() int
 }
 
-type ConfigurationDirective struct {
-	Kind      string           `yaml:"kind"`
-	Metadata  *CommentMetadata `yaml:",inline"`
-	Name      string           `yaml:"name"`
-	Parameter string           `yaml:"parameter"`
+type CommentDirective struct {
+	Kind     string          `yaml:"kind"`
+	Metadata CommentMetadata `yaml:",inline"`
 }
+
+func (d CommentDirective) ToSeclang() string {
+	return d.Metadata.ToSeclang()
+}
+
+type ConfigurationDirective struct {
+	Kind      string                     `yaml:"kind"`
+	Metadata  *CommentMetadata           `yaml:",inline"`
+	Name      ConfigurationDirectiveType `yaml:"name"`
+	Parameter string                     `yaml:"parameter"`
+}
+
+type ConfigurationDirectiveType string
+
+const (
+	SecAuditLogStorageDir         ConfigurationDirectiveType = "SecAuditLogStorageDir"
+	SecAuditLogDirMode            ConfigurationDirectiveType = "SecAuditLogDirMode"
+	SecAuditEngine                ConfigurationDirectiveType = "SecAuditEngine"
+	SecAuditLogFileMode           ConfigurationDirectiveType = "SecAuditLogFileMode"
+	SecAuditLog2                  ConfigurationDirectiveType = "SecAuditLog2"
+	SecAuditLog                   ConfigurationDirectiveType = "SecAuditLog"
+	SecAuditLogFormat             ConfigurationDirectiveType = "SecAuditLogFormat"
+	SecAuditLogParts              ConfigurationDirectiveType = "SecAuditLogParts"
+	SecAuditLogRelevantStatus     ConfigurationDirectiveType = "SecAuditLogRelevantStatus"
+	SecAuditLogType               ConfigurationDirectiveType = "SecAuditLogType"
+	SecUploadKeepFiles            ConfigurationDirectiveType = "SecUploadKeepFiles"
+	SecTmpSaveUploadedFiles       ConfigurationDirectiveType = "SecTmpSaveUploadedFiles"
+	SecUploadDir                  ConfigurationDirectiveType = "SecUploadDir"
+	SecUploadFileLimit            ConfigurationDirectiveType = "SecUploadFileLimit"
+	SecUploadFileMode             ConfigurationDirectiveType = "SecUploadFileMode"
+	SecComponentSignature         ConfigurationDirectiveType = "SecComponentSignature"
+	SecServerSignature            ConfigurationDirectiveType = "SecServerSignature"
+	SecWebAppId                   ConfigurationDirectiveType = "SecWebAppId"
+	SecMarker                     ConfigurationDirectiveType = "SecMarker"
+	SecConnEngine                 ConfigurationDirectiveType = "SecConnEngine"
+	SecContentInjection           ConfigurationDirectiveType = "SecContentInjection"
+	SecArgumentsLimit             ConfigurationDirectiveType = "SecArgumentsLimit"
+	SecDebugLog                   ConfigurationDirectiveType = "SecDebugLog"
+	SecDebugLogLevel              ConfigurationDirectiveType = "SecDebugLogLevel"
+	SecGeoLookupDb                ConfigurationDirectiveType = "SecGeoLookupDb"
+	SecGsbLookupDb                ConfigurationDirectiveType = "SecGsbLookupDb"
+	SecPcreMatchLimit             ConfigurationDirectiveType = "SecPcreMatchLimit"
+	SecPcreMatchLimitRecursion    ConfigurationDirectiveType = "SecPcreMatchLimitRecursion"
+	SecRequestBodyJsonDepthLimit  ConfigurationDirectiveType = "SecRequestBodyJsonDepthLimit"
+	SecRequestBodyAccess          ConfigurationDirectiveType = "SecRequestBodyAccess"
+	SecRequestBodyInMemoryLimit   ConfigurationDirectiveType = "SecRequestBodyInMemoryLimit"
+	SecRequestBodyLimit           ConfigurationDirectiveType = "SecRequestBodyLimit"
+	SecRequestBodyLimitAction     ConfigurationDirectiveType = "SecRequestBodyLimitAction"
+	SecRequestBodyNoFilesLimit    ConfigurationDirectiveType = "SecRequestBodyNoFilesLimit"
+	SecResponseBodyMimeType       ConfigurationDirectiveType = "SecResponseBodyMimeType"
+	SecResponseBodyMimeTypesClear ConfigurationDirectiveType = "SecResponseBodyMimeTypesClear"
+	SecResponseBodyAccess         ConfigurationDirectiveType = "SecResponseBodyAccess"
+	SecResponseBodyLimit          ConfigurationDirectiveType = "SecResponseBodyLimit"
+	SecResponseBodyLimitAction    ConfigurationDirectiveType = "SecResponseBodyLimitAction"
+	SecRuleEngine                 ConfigurationDirectiveType = "SecRuleEngine"
+	SecCookieFormat               ConfigurationDirectiveType = "SecCookieFormat"
+	SecCookieV0Separator          ConfigurationDirectiveType = "SecCookieV0Separator"
+	SecDataDir                    ConfigurationDirectiveType = "SecDataDir"
+	SecStatusEngine               ConfigurationDirectiveType = "SecStatusEngine"
+	SecTmpDir                     ConfigurationDirectiveType = "SecTmpDir"
+	SecUnicodeMapFile             ConfigurationDirectiveType = "SecUnicodeMapFile"
+	SecArgumentSeparator          ConfigurationDirectiveType = "SecArgumentSeparator"
+	SecChrootDir                  ConfigurationDirectiveType = "SecChrootDir"
+	SecCollectionTimeout          ConfigurationDirectiveType = "SecCollectionTimeout"
+	SecConnReadStateLimit         ConfigurationDirectiveType = "SecConnReadStateLimit"
+	SecConnWriteStateLimit        ConfigurationDirectiveType = "SecConnWriteStateLimit"
+	SecDisableBackendCompression  ConfigurationDirectiveType = "SecDisableBackendCompression"
+	SecGuardianLog                ConfigurationDirectiveType = "SecGuardianLog"
+	SecHashEngine                 ConfigurationDirectiveType = "SecHashEngine"
+	SecHashKey                    ConfigurationDirectiveType = "SecHashKey"
+	SecHashParam                  ConfigurationDirectiveType = "SecHashParam"
+	SecHashMethodRx               ConfigurationDirectiveType = "SecHashMethodRx"
+	SecHashMethodPm               ConfigurationDirectiveType = "SecHashMethodPm"
+	SecHttpBlKey                  ConfigurationDirectiveType = "SecHttpBlKey"
+	SecInterceptOnError           ConfigurationDirectiveType = "SecInterceptOnError"
+	SecRemoteRulesFailAction      ConfigurationDirectiveType = "SecRemoteRulesFailAction"
+	SecRuleInheritance            ConfigurationDirectiveType = "SecRuleInheritance"
+	SecRulePerfTime               ConfigurationDirectiveType = "SecRulePerfTime"
+	SecSensorId                   ConfigurationDirectiveType = "SecSensorId"
+	SecStreamInBodyInspection     ConfigurationDirectiveType = "SecStreamInBodyInspection"
+	SecStreamOutBodyInspection    ConfigurationDirectiveType = "SecStreamOutBodyInspection"
+	SecXmlExternalEntity          ConfigurationDirectiveType = "SecXmlExternalEntity"
+	SecCacheTransformations       ConfigurationDirectiveType = "SecCacheTransformations"
+)
+
+var (
+	configDirectiveTypes = map[string]ConfigurationDirectiveType{
+		"SecAuditLogStorageDir":         SecAuditLogStorageDir,
+		"SecAuditLogDirMode":            SecAuditLogDirMode,
+		"SecAuditEngine":                SecAuditEngine,
+		"SecAuditLogFileMode":           SecAuditLogFileMode,
+		"SecAuditLog2":                  SecAuditLog2,
+		"SecAuditLog":                   SecAuditLog,
+		"SecAuditLogFormat":             SecAuditLogFormat,
+		"SecAuditLogParts":              SecAuditLogParts,
+		"SecAuditLogRelevantStatus":     SecAuditLogRelevantStatus,
+		"SecAuditLogType":               SecAuditLogType,
+		"SecUploadKeepFiles":            SecUploadKeepFiles,
+		"SecTmpSaveUploadedFiles":       SecTmpSaveUploadedFiles,
+		"SecUploadDir":                  SecUploadDir,
+		"SecUploadFileLimit":            SecUploadFileLimit,
+		"SecUploadFileMode":             SecUploadFileMode,
+		"SecComponentSignature":         SecComponentSignature,
+		"SecServerSignature":            SecServerSignature,
+		"SecWebAppId":                   SecWebAppId,
+		"SecMarker":                     SecMarker,
+		"SecConnEngine":                 SecConnEngine,
+		"SecContentInjection":           SecContentInjection,
+		"SecArgumentsLimit":             SecArgumentsLimit,
+		"SecDebugLog":                   SecDebugLog,
+		"SecDebugLogLevel":              SecDebugLogLevel,
+		"SecGeoLookupDb":                SecGeoLookupDb,
+		"SecGsbLookupDb":                SecGsbLookupDb,
+		"SecPcreMatchLimit":             SecPcreMatchLimit,
+		"SecPcreMatchLimitRecursion":    SecPcreMatchLimitRecursion,
+		"SecRequestBodyJsonDepthLimit":  SecRequestBodyJsonDepthLimit,
+		"SecRequestBodyAccess":          SecRequestBodyAccess,
+		"SecRequestBodyInMemoryLimit":   SecRequestBodyInMemoryLimit,
+		"SecRequestBodyLimit":           SecRequestBodyLimit,
+		"SecRequestBodyLimitAction":     SecRequestBodyLimitAction,
+		"SecRequestBodyNoFilesLimit":    SecRequestBodyNoFilesLimit,
+		"SecResponseBodyMimeType":       SecResponseBodyMimeType,
+		"SecResponseBodyMimeTypesClear": SecResponseBodyMimeTypesClear,
+		"SecResponseBodyAccess":         SecResponseBodyAccess,
+		"SecResponseBodyLimit":          SecResponseBodyLimit,
+		"SecResponseBodyLimitAction":    SecResponseBodyLimitAction,
+		"SecRuleEngine":                 SecRuleEngine,
+		"SecCookieFormat":               SecCookieFormat,
+		"SecCookieV0Separator":          SecCookieV0Separator,
+		"SecDataDir":                    SecDataDir,
+		"SecStatusEngine":               SecStatusEngine,
+		"SecTmpDir":                     SecTmpDir,
+		"SecUnicodeMapFile":             SecUnicodeMapFile,
+		"SecArgumentSeparator":          SecArgumentSeparator,
+		"SecChrootDir":                  SecChrootDir,
+		"SecCollectionTimeout":          SecCollectionTimeout,
+		"SecConnReadStateLimit":         SecConnReadStateLimit,
+		"SecConnWriteStateLimit":        SecConnWriteStateLimit,
+		"SecDisableBackendCompression":  SecDisableBackendCompression,
+		"SecGuardianLog":                SecGuardianLog,
+		"SecHashEngine":                 SecHashEngine,
+		"SecHashKey":                    SecHashKey,
+		"SecHashParam":                  SecHashParam,
+		"SecHashMethodRx":               SecHashMethodRx,
+		"SecHashMethodPm":               SecHashMethodPm,
+		"SecHttpBlKey":                  SecHttpBlKey,
+		"SecInterceptOnError":           SecInterceptOnError,
+		"SecRemoteRulesFailAction":      SecRemoteRulesFailAction,
+		"SecRuleInheritance":            SecRuleInheritance,
+		"SecRulePerfTime":               SecRulePerfTime,
+		"SecSensorId":                   SecSensorId,
+		"SecStreamInBodyInspection":     SecStreamInBodyInspection,
+		"SecStreamOutBodyInspection":    SecStreamOutBodyInspection,
+		"SecXmlExternalEntity":          SecXmlExternalEntity,
+		"SecCacheTransformations":       SecCacheTransformations,
+	}
+)
 
 func NewConfigurationDirective() *ConfigurationDirective {
 	c := new(ConfigurationDirective)
@@ -26,19 +183,19 @@ func NewConfigurationDirective() *ConfigurationDirective {
 	return c
 }
 
+func (c *ConfigurationDirective) SetName(name string) error {
+	constName, ok := configDirectiveTypes[name]
+	if !ok {
+		return fmt.Errorf("Invalid configuration directive name: %s", name)
+	}
+	c.Name = constName
+	return nil
+}
+
 func (c ConfigurationDirective) GetMetadata() Metadata {
 	return c.Metadata
 }
 
 func (c ConfigurationDirective) ToSeclang() string {
-	return c.Metadata.Comment + c.Name + " " + c.Parameter + "\n"
-}
-
-type CommentDirective struct {
-	Kind     string          `yaml:"kind"`
-	Metadata CommentMetadata `yaml:",inline"`
-}
-
-func (d CommentDirective) ToSeclang() string {
-	return d.Metadata.ToSeclang()
+	return c.Metadata.Comment + string(c.Name) + " " + c.Parameter + "\n"
 }
